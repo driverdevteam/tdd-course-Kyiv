4983bf5f9200e3eb41ba91c3661e295d339c48a8
prepas <prepa@ukr.net>
2018-11-14 18:28:06 +0200

    add reverse task

---
+++ tdd_intro/homework/00_intro/test.cpp
@@ -0,0 +1,9 @@
+/*
+Reverse a string
+
+For example: input: "cool" output: "looc"
+             input: "12345" output: "54321"
+             input: "123456" output: "654321"
+*/
+
+#include <gtest/gtest.h>
