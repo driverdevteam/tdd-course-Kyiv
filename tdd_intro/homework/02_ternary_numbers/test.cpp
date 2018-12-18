ead3e320d3245980bac4e540e82ce1ff06278086
Viacheslav Konstantinov <konstantinov.slava@apriorit.com>
2018-12-18 23:58:14 +0200

    Practical class

--- tdd_intro/homework/02_ternary_numbers/test.cpp
+++ tdd_intro/homework/02_ternary_numbers/test.cpp
@@ -16,3 +16,77 @@ The last place in a ternary number is the 1's place. The second to last is the 3
 
 If your language provides a method in the standard library to perform the conversion, pretend it doesn't exist and implement it yourself.
 */
+
+/*
+ * 1) 0 test
+ * 2) 1 test
+ * 3) 2 test
+ * 4) invalid digit test
+ * 5) Multiple digits tests
+ * 6) Invalid multiple digits tests
+ * 7) Acceptence test
+ */
+
+bool TernaryNumber(int number)
+{
+    return number < 3;
+}
+
+int ConvertTernaryNumbers(const std::string &number)
+{
+    int sum = 0;
+    for (int index = 0; index < number.size(); ++index)
+    {
+        int numberValue = std::stoi(std::string(1, number[number.size() - 1 - index]));
+        if (!TernaryNumber(numberValue))
+        {
+            return 0;
+        }
+        sum += numberValue * std::pow(3, index);
+    }
+
+    return sum;
+}
+
+TEST(TernaryNumbers, ZeroTest)
+{
+    ASSERT_TRUE(TernaryNumber(0));
+}
+
+TEST(TernaryNumbers, CheckOne)
+{
+    ASSERT_TRUE(TernaryNumber(1));
+}
+
+TEST(TernaryNumbers, CheckTwo)
+{
+    ASSERT_TRUE(TernaryNumber(2));
+}
+
+TEST(TernaryNumbers, InvalidDigit)
+{
+    ASSERT_FALSE(TernaryNumber(3));
+}
+
+TEST(TernaryNumbers, MultipleDigits)
+{
+    ASSERT_EQ(0, ConvertTernaryNumbers("00"));
+}
+
+TEST(TernaryNumbers, MultipleDigits111)
+{
+    ASSERT_EQ(13, ConvertTernaryNumbers("111"));
+}
+
+TEST(TernaryNumbers, InvalidMultipleDigits)
+{
+    ASSERT_EQ(0, ConvertTernaryNumbers("113"));
+}
+
+TEST(TernaryNumbers, Acceptance)
+{
+    ASSERT_EQ(0, ConvertTernaryNumbers("123"));
+    ASSERT_EQ(16, ConvertTernaryNumbers("121"));
+    ASSERT_EQ(439, ConvertTernaryNumbers("121021"));
+    ASSERT_EQ(597871, ConvertTernaryNumbers("1010101010101"));
+}
