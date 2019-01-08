7caf78f351ef6266d9e5901e44e0398659aed047
Viacheslav Konstantinov <konstantinov.slava@apriorit.com>
2019-01-08 23:24:31 +0200

    Get max wind speed red test

--- tdd_intro/homework/04_weather_client/test.cpp
+++ tdd_intro/homework/04_weather_client/test.cpp
@@ -365,3 +365,11 @@ TEST(WeatherClient, GetAverageWindDirectionAcceptence)
     EXPECT_THROW(client.GetAverageWindDirection(server, "sdsdfdf"), std::runtime_error);
     EXPECT_ANY_THROW(client.GetAverageWindDirection(server, "01.11.2018")); // Использовал этот метод, почему то 2 метода EXPECT_THROW не компилируются в одном методе...
 }
+
+TEST(WeatherClient, GetMaximumWindSpeedFor31_08_2018)
+{
+    WeatherServerStub server;
+    WeatherClient client;
+
+    ASSERT_FLOAT_EQ(5.1f, client.GetMaximumWindSpeed(server, "31.08.2018"));
+}
